df8da2b598ac0dabf0e72f85a7a84bea08def1a4
Theerut Sirichai <33027630+SrT-H@users.noreply.github.com>
2017-11-18 02:18:32 +0700

    Create class
    -

--- demo.py
+++ demo.py
@@ -3,35 +3,37 @@ import numpy as np
 from sklearn.svm import SVR
 import matplotlib.pyplot as plt
 
-#plt.switch_backend('Qt5Agg')  
+class set50:
 
-dates = []
-prices = []
+    #plt.switch_backend('Qt5Agg')
+    def __init__(self):
+        self.dates = []
+        self.prices = []
 
-def get_data(filename):
-    with open(filename, 'r') as csvfile:
-        csvFileReader = csv.reader(csvfile)
-        next(csvFileReader)	# skipping column names
-        for row in csvFileReader:
-            dates.append((row[0]))
-            prices.append(float(row[5]))
-    return
+    def get_data(self, filename):
+        with open(filename, 'r') as csvfile:
+            csvFileReader = csv.reader(csvfile)
+            next(csvFileReader)	# skipping column names
+            for row in csvFileReader:
+                self.dates.append((row[0]))
+                self.prices.append(float(row[5]))
+        return
 
-def predict_price(dates, prices, x):
-    dates = np.reshape(dates,(len(dates), 1)) # converting to matrix of n X 1
+    def predict_price(self, dates, prices, x):
+        self.dates = np.reshape(dates,(len(dates), 1)) # converting to matrix of n X 1
 
-    plt.scatter(dates, prices, color= 'black', label= 'Data') # plotting the initial datapoints 
-    plt.xlabel('Date')
-    plt.ylabel('Price')
-    plt.title('SET50 Graph')
-    plt.legend()
-    plt.show()
+        plt.scatter(self.dates, prices, color= 'black', label= 'Data') # plotting the initial datapoints
+        plt.xlabel('Date')
+        plt.ylabel('Price')
+        plt.title('SET50 Graph')
+        plt.legend()
+        plt.show()
 
-def create_graph():
-    get_data('S50Z171440.csv') # calling get_data method by passing the csv file to it
-    print("Dates- ", dates)
-    print("Prices- ", prices)
+    def create_graph(self):
+        self.get_data('S50Z171440.csv') # calling get_data method by passing the csv file to it
+        print("Dates- ", self.dates)
+        print("Prices- ", self.prices)
 
-    predicted_price = predict_price(dates, prices, 29)
-    print(predicted_price)
+        predicted_price = self.predict_price(self.dates, self.prices, 29)
+        print(predicted_price)
 
