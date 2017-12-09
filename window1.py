6ac2c5247a789212b18a2a29f239f45994658532
Theerut Sirichai <33027630+SrT-H@users.noreply.github.com>
2017-12-09 14:24:59 +0700

    temp

--- window1.py
+++ window1.py
@@ -9,7 +9,7 @@ class main_window:
         self.window = tk.Tk()
         self.window.resizable(width=False, height=False) #fix window size
         self.window.geometry('{}x{}'.format(1080, 760)) #window size
-        self.window.title("Welcome!")
+        self.window.title("Welcome")
         # get screen width and height
         ws = self.window.winfo_screenwidth()  # width of the screen
         hs = self.window.winfo_screenheight()  # height of the screen
@@ -23,16 +23,23 @@ class main_window:
         self.window.geometry('%dx%d+%d+%d' % (800, 600, self.x, self.y))
 
         #Text
-        label = tk.Label(self.window, text="Welcome \nto \nStock Market Trade Analysis.", font=("Helvetica", 32)).place(x=120, y=30)
+        label = tk.Label(self.window, text="Welcom to \nlittle worm PSIT project",\
+                         font=("Helvetica", 32)).place(x=180, y=50)
 
         #Button
-        graph_button = tk.Button(self.window, text='Graph', font=("Helvetica", 20), bg='green',  width=10, height=1,\
-                            command=self.graph_action).place(x=320, y=320)
+        graph_button = tk.Button(self.window, text='Graphs', font=("Helvetica", 20), bg='#faf800', width=10, height=1, \
+                                 command=self.graph_action).place(x=300, y=260)
+        fill_button = tk.Button(self.window, text='About us', font=("Helvetica", 20), bg='#00b300', width=10, height=1,\
+                            command=self.about_us).place(x=300, y=400)
         exit_button = tk.Button(self.window, text='EXIT', font=("Helvetica", 20, 'bold'), bg='red', width=5, height=1,\
                             command=self.exit_action).place(x=680, y=520)
 
         self.window.size()
         self.window.mainloop()
+
+    def about_us(self):
+        #insert information
+        print("About us")
 
     def graph_action(self):
         #show graph
@@ -49,7 +56,7 @@ class main_window:
             pass
 
 class showing_gragh:
-    ''' SHOWING GRAPH '''
+    ''' FILL IN THE BOX'''
     def __init__(self):
         #Window
         self.window = tk.Tk()
@@ -74,7 +81,7 @@ class showing_gragh:
         #Button
         back_button = tk.Button(self.window, text='<<', font=("Helvetica", 20, 'bold'), width=5, height=1, \
                             command=self.back_action).place(x=30, y=520)
-        showing_graph = tk.Button(self.window, text='Set 50', font=("Helvetica", 20, 'bold'), width=6, height=1, \
+        showing_graph = tk.Button(self.window, text='Set50', font=("Helvetica", 20, 'bold'), width=6, height=1, \
                                 command=self.set50).place(x=100, y=100)
         exit_button = tk.Button(self.window, text='EXIT', font=("Helvetica", 20, 'bold'), bg='red', width=5, height=1, \
                                 command=self.exit_action).place(x=680, y=520)
@@ -82,7 +89,6 @@ class showing_gragh:
         self.window.mainloop()
 
     def set50(self):
-        #create set50 graph
         temp = demo.set50()
         temp.create_graph()
 
