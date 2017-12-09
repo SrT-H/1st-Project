d7628077576e8345f0f200c110b92b7522124467
Theerut Sirichai <33027630+SrT-H@users.noreply.github.com>
2017-12-09 14:55:07 +0700

    Config

--- window1.py
+++ window1.py
@@ -27,26 +27,21 @@ class main_window:
                          font=("Helvetica", 32)).place(x=180, y=50)
 
         #Button
-        graph_button = tk.Button(self.window, text='Graphs', font=("Helvetica", 20), bg='#faf800', width=10, height=1, \
+        graph_button = tk.Button(self.window, text='Graphs', font=("Helvetica", 20), bg='#ffff66', width=10, height=1, \
                                  command=self.graph_action).place(x=300, y=260)
-        fill_button = tk.Button(self.window, text='About us', font=("Helvetica", 20), bg='#00b300', width=10, height=1,\
-                            command=self.about_us).place(x=300, y=400)
-        exit_button = tk.Button(self.window, text='EXIT', font=("Helvetica", 20, 'bold'), bg='red', width=5, height=1, \
+        fill_button = tk.Button(self.window, text='About us', font=("Helvetica", 20), bg='#66ff66', width=10, height=1,\
+                            command=self.about).place(x=300, y=400)
+        exit_button = tk.Button(self.window, text='EXIT', font=("Helvetica", 20, 'bold'), bg='#ff4d4d', width=5, height=1, \
                                 command=self.exit_action).place(x=680, y=520)
-
-        #Label
-        label = tk.Label(self.window, text="Welcom to python", font=("Helvetica", 32)).place(x=230, y=10)
 
         self.window.size()
         self.window.mainloop()
 
-    def about_us(self):
+    def about(self):
         #insert information
         print("About us")
-
-    def fill_action(self):
-        #insert information
-        print("Fill in the blank.")
+        self.window.destroy()
+        about_us()
 
     def graph_action(self):
         #show graph
@@ -117,4 +112,56 @@ class showing_gragh:
         else:
             print("Continue")
             pass
+
+class about_us:
+    ''' ABOUT US'''
+    def __init__(self):
+        #Window
+        self.window = tk.Tk()
+        self.window.resizable(width=False, height=False) #fix window size
+        self.window.geometry('{}x{}'.format(1080, 760)) #window size
+        self.window.title("About us")
+        # get screen width and height
+        ws = self.window.winfo_screenwidth()  # width of the screen
+        hs = self.window.winfo_screenheight()  # height of the screen
+
+        # calculate x and y coordinates for the Tk root window
+        self.x = (ws / 2) - (1080 / 2)
+        self.y = (hs / 2) - (760 / 2)
+
+        # set the dimensions of the screen
+        # and where it is placed
+        self.window.geometry('%dx%d+%d+%d' % (800, 600, self.x, self.y))
+
+        # Text
+        label = tk.Label(self.window, text="About us", font=("Helvetica", 32)).place(x=350, y=10)
+
+        #Button
+        back_button = tk.Button(self.window, text='<<', font=("Helvetica", 20, 'bold'), width=5, height=1, \
+                                command=self.back_action).place(x=30, y=520)
+        exit_button = tk.Button(self.window, text='EXIT', font=("Helvetica", 20, 'bold'), bg='red', width=5, height=1, \
+                                command=self.exit_action).place(x=680, y=520)
+
+        self.window.size()
+        self.window.mainloop()
+
+    def back_action(self):
+        # back to main menu
+        self.window.destroy()
+        main_window()
+        print("back to main menu")
+
+    def exit_action(self):
+        # exit
+        result = tkinter.messagebox.askquestion("Exit", "Do you want to exit?", icon='warning')
+        if result == 'yes':
+            print("EXIT")
+            self.window.destroy()
+        else:
+            print("Continue")
+            pass
+
+
+
+#call main window
 main_window()
